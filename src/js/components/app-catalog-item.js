49ee29f1ff6275ca34a288eee2eaebded5011bc6
Danny Swaby <danny@swa.by>
2016-02-28 16:37:45 -0600

    added Catalog Item Component

---
+++ src/js/components/app-catalog-item.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import AppActions from '../actions/app-actions'
+import CartButton from './app-cart-button'
+
+export default (props) => {
+	return (
+		<div className="col-xs-6 col-med-3 col-sm-4">
+			<h4> { props.item.title }</h4>
+			<img src="http://placehold.it/250x250" width="100%" className="img-responsive" />
+			<p>{ props.item.summary}</p>
+			<p>{ props.item.cost }</p>
+			<CartButton
+				handler={
+					AppActions.addItem.bind( null, props.item )
+				}
+				/>
+		</div>
+	)
+}
